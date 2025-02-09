02f9412aa12097883241b1787d419d2d8dec4cbf
Raquelfes11 <202209790@alu.comillas.edu>
2025-02-10 00:32:56 +0100

    Data Processing

--- src/data_processing.py
+++ src/data_processing.py
@@ -41,12 +41,17 @@ def create_lookup_tables(words: List[str]) -> Tuple[Dict[str, int], Dict[int, st
     # TODO
     word_counts: Counter = Counter(words)
     # Sorting the words from most to least frequent in text occurrence.
-    sorted_words = [word for word, count in word_counts.most_common()]
+    sorted_words:List[str] = [word for word, _ in word_counts.most_common()]
     sorted_vocab: List[int] = [words.index(word) for word in sorted_words]
 
     # Create int_to_vocab and vocab_to_int dictionaries.
-    int_to_vocab: Dict[int, str] = {i: words[i] for i in range(len(sorted_vocab))}
-    vocab_to_int: Dict[str, int] = {words[i]: i for i in range(len(sorted_vocab))}
+    int_to_vocab: Dict[int, str] = dict()
+    i:int = 0
+    for index in sorted_vocab:
+        int_to_vocab[i] = words[index]
+        i += 1
+
+    vocab_to_int: Dict[str, int] = {word: i for i, word in int_to_vocab.items()}
 
     return vocab_to_int, int_to_vocab
 
@@ -72,7 +77,7 @@ def subsample_words(words: List[str], vocab_to_int: Dict[str, int], threshold: f
     """
     # TODO
     # Convert words to integers
-    int_words: List[int] = None
+    int_words: List[int] = [vocab_to_int[word] for word in words]
     
     freqs: Dict[str, float] = None
     train_words: List[str] = None
--- src/p.py
+++ src/p.py
@@ -7,6 +7,7 @@ try:
 except ImportError:
     from utils import tokenize
 
+
 def load_and_preprocess_data(infile: str) -> List[str]:
     """
     Load text data from a file and preprocess it using a tokenize function.
@@ -26,6 +27,41 @@ def load_and_preprocess_data(infile: str) -> List[str]:
 
     return tokens
 
+def create_lookup_tables(words: List[str]) -> Tuple[Dict[str, int], Dict[int, str]]:
+    """
+    Create lookup tables for vocabulary.
 
-tokens = load_and_preprocess_data("data/text8")
-print(tokens)
+    Args:
+        words: A list of words from which to create vocabulary.
+
+    Returns:
+        A tuple containing two dictionaries. The first dictionary maps words to integers (vocab_to_int),
+        and the second maps integers to words (int_to_vocab).
+    """
+    # TODO
+    word_counts: Counter = Counter(words)
+    # Sorting the words from most to least frequent in text occurrence.
+    sorted_words:List[str] = [word for word, _ in word_counts.most_common()]
+    sorted_vocab: List[int] = [words.index(word) for word in sorted_words]
+
+    # Create int_to_vocab and vocab_to_int dictionaries.
+    int_to_vocab: Dict[int, str] = dict()
+    i:int = 0
+    for index in sorted_vocab:
+        int_to_vocab[i] = words[index]
+        i += 1
+
+    vocab_to_int: Dict[str, int] = {word: i for i, word in int_to_vocab.items()}
+    print(vocab_to_int)
+    print(int_to_vocab)
+    print(sorted_vocab)
+    return vocab_to_int, int_to_vocab
+
+
+
+
+
+
+
+#tokens = load_and_preprocess_data("data/text8")
+create_lookup_tables(["hello", "world", "hello", "test"])
